103361092c8a477e9d829fc1cc57438d582c4060
Your Name <you@example.com>
2017-05-21 19:22:55 +0200

    add languages en

---
+++ form.php
@@ -0,0 +1,37 @@
+<!doctype html>
+
+<html lang="pl">
+<head>
+  <title>NewsyLotnicze.eu</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Free Web tutorials">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="author" content="Seba">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- ONLY FONTS  -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon/favicon-32x32.png" type="image/x-icon" />
+  <script>
+  <!-- Miejsce na javascript-->
+  </script>
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<section class="info-forum">
+	<div class="container">
+		<div class="row">
+			<div class="info-forum-box">
+			<p class="info-forum-p">Dziękujęmy , zapraszamy ponownie ! </p>
+			</div>
+		</div>
+	</div>
+</section>
+
+</body>
+
+</html>
